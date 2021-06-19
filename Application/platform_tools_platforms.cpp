1da9270430cbb6222d618bc99f2be710ef3735a4
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2021-06-19 16:14:57 +0200

    fixed Dialogs

--- Application/platform_tools_platforms.cpp
+++ Application/platform_tools_platforms.cpp
@@ -28,119 +28,134 @@ std::string wstrtostr(const std::wstring& wstr)
     return strTo;
 }
 
-const char* BuildFilter(std::vector<std::pair<std::string, std::string>> filterValues)
+std::wstring strTowstr(const std::string& str)
 {
+    std::wstring strTo;
+    wchar_t* szTo = new wchar_t[str.length() + 1];
+    szTo[str.length()] = '\0';
+    MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, szTo, (int)str.length());
+    strTo = szTo;
+    delete[] szTo;
+    return strTo;
+}
+
+void copyWCharsRaw(wchar_t* dst, const wchar_t* src, size_t amt) {
+    for (size_t i = 0; i < amt; i++) {
+        dst[i] = src[i];
+    }
+}
+
+const wchar_t* BuildFilter(std::vector<std::pair<std::string, std::string>> filterValues) {
+    if (filterValues.size() == 0)
+    {
+        filterValues.push_back({"All Files","*"});
+    }
+    
     size_t count = 0;
     for (auto& filterValue : filterValues)
     {
         count += filterValue.first.size() + 1;
-        count += filterValue.first.size() + 3;
+        count += filterValue.second.size() + 1 + 2; // the 2 for the "*." added later
     }
-    count += 1;
-    if (count == 1)
+    count += 1; // second null
+
+    wchar_t* filter = new wchar_t[count];
+
+    size_t offset = 0;
+    for (auto& filterValue : filterValues)
     {
-        count += strlen("All Files\t*.*\t");
-    }
+        const std::wstring& name = strTowstr(filterValue.first);
+        const std::wstring extension = strTowstr("*." + filterValue.second);
 
-    char* filter = new char[count];
+        copyWCharsRaw(filter + offset, name.c_str(), name.length());
+        offset += name.length();
 
-    if (filterValues.size() == 0)
-    {
-        memcpy(filter, "All Files\0*.*\0\0", count);
-    }
-    else
-    {
-        size_t offset = 0;
-        for (auto& filterValue : filterValues)
-        {
-            const std::string& name = filterValue.first;
-            std::string extension = "*." + filterValue.second;
+        filter[offset] = 0;
+        offset += 1;
 
-            memcpy(filter + offset, name.c_str(), name.size());
-            offset += name.size();
+        copyWCharsRaw(filter + offset, extension.c_str(), extension.length());
+        offset += extension.length();
 
-            memcpy(filter + offset, "\0", 1);
-            offset += 1;
-
-            memcpy(filter + offset, extension.c_str(), extension.size());
-            offset += extension.size();
-
-            memcpy(filter + offset, "\0", 1);
-            offset += 1;
-        }
-        memcpy(filter + offset, "\0", 1);
+        filter[offset] = 0;
         offset += 1;
     }
+    filter[offset] = 0;
+    offset += 1;
 
     return filter;
 }
 
-std::string OpenFileDialog(const char* filename, std::vector<std::pair<std::string,std::string>> filterValues)
+OPENFILENAME getDefaultOFN(LPWSTR lpstrFile,const DWORD nMaxFile, LPCWSTR lpstrFilter)
 {
-    std::string result;
-
-    const char* filter = BuildFilter(filterValues);
-
     // common dialog box structure, setting all fields to 0 is important
     OPENFILENAME ofn = { 0 };
-    TCHAR szFile[260] = { 0 };
-    if (filename != nullptr)
-        strcpy_s(szFile, 260, filename);
 
     // Initialize remaining fields of OPENFILENAME structure
     ofn.lStructSize = sizeof(ofn);
     ofn.hwndOwner = WindowHandle;
-    ofn.lpstrFile = szFile;
-    ofn.nMaxFile = sizeof(szFile);
-    ofn.lpstrFilter = filter;
+    ofn.lpstrFile = lpstrFile;
+    ofn.nMaxFile = nMaxFile;
+    ofn.lpstrFilter = lpstrFilter;
     ofn.nFilterIndex = 1;
     ofn.lpstrFileTitle = nullptr;
     ofn.nMaxFileTitle = 0;
     ofn.lpstrInitialDir = nullptr;
     ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+    return ofn;
+}
+
+std::string OpenFileDialog(const char* filename, std::vector<std::pair<std::string, std::string>> filterValues)
+{
+    std::string result;
+
+    const wchar_t* filter = BuildFilter(filterValues);
+
+    constexpr int nMaxFile = 260;
+    TCHAR szFile[nMaxFile] = { 0 };
+    if (filename != nullptr)
+    {
+        std::wstring ws = strTowstr(std::string(filename));
+        wcsncpy(szFile, ws.c_str(), nMaxFile);
+        szFile[nMaxFile - 1] = 0; //safety null termination
+    }
+
+    OPENFILENAME ofn = getDefaultOFN(szFile, nMaxFile, filter);
 
     if (GetOpenFileName(&ofn) == TRUE)
     {
         // use ofn.lpstrFile here
-        result = ofn.lpstrFile;
+        result = wstrTostr(std::wstring(ofn.lpstrFile));
     }
 
-    delete[](filter);
+    delete[] filter;
 
     return result;
 }
-
 std::string SaveFileDialog(const char* filename, std::vector<std::pair<std::string, std::string>> filterValues)
 {
     std::string result;
 
-    const char* filter = BuildFilter(filterValues);
-
-    // common dialog box structure, setting all fields to 0 is important
-    OPENFILENAME ofn = { 0 };
-    TCHAR szFile[260] = { 0 };
+    const wchar_t* filter = BuildFilter(filterValues);
+    
+    constexpr int nMaxFile = 260;
+    TCHAR szFile[nMaxFile] = { 0 };
     if (filename != nullptr)
-        strcpy_s(szFile, filename);
+    {
+        std::wstring ws = strTowstr(std::string(filename));
+        wcsncpy(szFile, ws.c_str(), nMaxFile);
+        szFile[nMaxFile - 1] = 0; //safety null termination
+    }
 
-    // Initialize remaining fields of OPENFILENAME structure
-    ofn.lStructSize = sizeof(ofn);
-    ofn.hwndOwner = WindowHandle;
-    ofn.lpstrFile = szFile;
-    ofn.nMaxFile = sizeof(szFile);
-    ofn.lpstrFilter = filter;
-    ofn.nFilterIndex = 1;
-    ofn.lpstrFileTitle = nullptr;
-    ofn.nMaxFileTitle = 0;
-    ofn.lpstrInitialDir = nullptr;
-    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+    OPENFILENAME ofn = getDefaultOFN(szFile, nMaxFile, filter);
 
     if (GetSaveFileName(&ofn) == TRUE)
     {
         // use ofn.lpstrFile here
-        result = ofn.lpstrFile;
+        result = wstrTostr(std::wstring(ofn.lpstrFile));
     }
 
-    delete[](filter);
+    delete[] filter;
 
     return result;
 }
